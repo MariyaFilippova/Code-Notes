6ca89943b92acb5d54e1077ff5e46c20d1a6a7f1
Maria Filippova <maxa.spb6@gmail.com>
2024-08-06 14:09:00 +0200

    add Inlay with suggestion to leave a note when code was selected

--- src/main/kotlin/org/me/notes/ui/NotesToolBar.kt
+++ src/main/kotlin/org/me/notes/ui/NotesToolBar.kt
@@ -1,10 +1,15 @@
 package org.me.notes.ui
 
+import com.intellij.codeInsight.daemon.impl.HintRenderer
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.Inlay
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseListener
+import com.intellij.openapi.editor.event.SelectionEvent
+import com.intellij.openapi.editor.event.SelectionListener
 import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.openapi.keymap.KeymapUtil
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiManager
 import com.intellij.ui.JBColor
@@ -19,36 +24,62 @@ class NotesToolBar(val editor: Editor, project: Project) : Disposable {
 
     private val myMouseListener = MyMouseListener()
     private val myKeyBoardListener = MyKeyBoardListener()
-    private var myMouseReleased = false
+    private val mySelectionListener = MySelectionListener()
+
+    private var myInlay: Inlay<HintRenderer>? = null
 
     init {
         editor.addEditorMouseListener(myMouseListener)
         editor.contentComponent.addKeyListener(myKeyBoardListener)
+        editor.selectionModel.addSelectionListener(mySelectionListener)
     }
 
     private inner class MyMouseListener : EditorMouseListener {
         override fun mouseReleased(event: EditorMouseEvent) {
-            myMouseReleased = true
-        }
-
-        override fun mouseClicked(event: EditorMouseEvent) {
-            myMouseReleased = false
+            myInlay?.dispose()
+            if (editor.selectionModel.hasSelection()) {
+                myInlay = editor.inlayModel.addAfterLineEndElement(
+                    editor.selectionModel.selectionEnd,
+                    false,
+                    HintRenderer("Press %s to leave note".format(KeymapUtil.getKeyText(VK_SHIFT)))
+                )
+            }
         }
     }
 
     private inner class MyKeyBoardListener : KeyAdapter() {
         override fun keyPressed(e: KeyEvent?) {
-            if (e?.keyCode == VK_SHIFT && myMouseReleased) {
+            myInlay?.dispose()
+            if (e?.keyCode == VK_SHIFT) {
                 if (myFile == null || !editor.selectionModel.hasSelection()) return
                 myHint.showHint()
             }
         }
+
+        override fun keyReleased(e: KeyEvent?) {
+            myInlay?.dispose()
+            if (e?.keyCode == VK_SHIFT) {
+                if (myFile == null || !editor.selectionModel.hasSelection()) return
+                myInlay = editor.inlayModel.addAfterLineEndElement(
+                    editor.selectionModel.selectionEnd,
+                    false,
+                    HintRenderer("Press shift to leave note")
+                )
+            }
+        }
+    }
+
+    private inner class MySelectionListener : SelectionListener {
+        override fun selectionChanged(e: SelectionEvent) {
+            if (!editor.selectionModel.hasSelection()) myInlay?.dispose()
+        }
     }
 
     override fun dispose() {
-        editor.removeEditorMouseListener(myMouseListener)
         editor.contentComponent.addKeyListener(myKeyBoardListener)
+        editor.selectionModel.removeSelectionListener(mySelectionListener)
         myHint.dispose()
+        myInlay?.dispose()
     }
 }
 
